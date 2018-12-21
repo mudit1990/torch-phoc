cc39f0902e6b2fde0a42d0e0f5c176a80a2ec2b5
Sanjay-Reddy-S <sanjayreddys07@gmail.com>
2018-12-20 20:23:14 -0500

    Adding the extend_boundary files

---
+++ src/extend_boundary.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+python extend_boundary.py D0042-1070001
+python extend_boundary.py D0042-1070002
+python extend_boundary.py D0042-1070006
+python extend_boundary.py D0042-1070007
+python extend_boundary.py D0117-5755018
+python extend_boundary.py D0117-5755035
+python extend_boundary.py D0117-5755036
+
+#python extend_boundary.py D0117-5755035
+#python extend_boundary.py D0117-5755036
+#python extend_boundary.py D5005-5028052
+#python extend_boundary.py D5005-5028054
+#python extend_boundary.py D5005-5028097
+#python extend_boundary.py D5005-5028100
+#python extend_boundary.py D5005-5028149
+
+#python extend_boundary.py D0006-0285025
+#python extend_boundary.py D0017-1592006
+#python extend_boundary.py D0041-5370006
+#python extend_boundary.py D0041-5370026
+#python extend_boundary.py D0042-1070001
+#python extend_boundary.py D0042-1070002
+#python extend_boundary.py D0042-1070003
+#python extend_boundary.py D0042-1070004
+#python extend_boundary.py D0042-1070005
+#python extend_boundary.py D0042-1070006
+#python extend_boundary.py D0042-1070007
+#python extend_boundary.py D0042-1070009
+#python extend_boundary.py D0042-1070010
+#python extend_boundary.py D0042-1070012
+
+##python extend_boundary.py D0042-1070013
+
+#python extend_boundary.py D0042-1070015
+#python extend_boundary.py D0079-0019007
+#python extend_boundary.py D0089-5235001
+#python extend_boundary.py D0090-5242001
+#python extend_boundary.py D0117-5755018
+#python extend_boundary.py D0117-5755024
+#python extend_boundary.py D0117-5755025
+#python extend_boundary.py D0117-5755033
+
--- src/extend_boundary2.py
+++ src/extend_boundary2.py
@@ -1,36 +1,55 @@
+"""
+This is an older version. Mainly for experiments. Use extend_boundary.py
+"""
 import numpy as np
 from scipy.misc import imsave
 from PIL import Image
 from scipy.ndimage import imread
-import sys
+import math
 
 #fN = 'D0006-0285025'
 #fN = 'D0117-5755025'
 #fN = 'D0117-5755024'
 #fN = 'D0117-5755018'
-#fN = 'D0090-5242001'
-fN = sys.argv[1]
+fN = 'D0090-5242001'
 
-#im_fN='../detection_outputs/'+fN+'.npy' #90 has 536 clips
-im_fN='../detection_outputs_lines/'+fN+'.npy' #90 has 536 clips. FOr ray_regionsand ray_labels (Now being used for _extendby2)
-#im_fN='../detection_outputs_lines_ext_2/'+fN+'.npy' #90 has 536 clips
-
-#align_fN='../detection_alignment/'+fN+'.npy' #90 has 536 entries
-align_fN='../detection_alignment_lines/'+fN+'.npy' #90 has 536 entries. FOr ray_labels and ray_regions (Now being used for _extendby2)
-#align_fN='../detection_alignment_lines_ext_2/'+fN+'.npy' #90 has 536 entries
-
+im_fN='../detection_outputs/'+fN+'.npy' #90 has 536 clips
+align_fN='../detection_alignment/'+fN+'.npy' #90 has 536 entries
 annot_fN='../jerods_annotations/'+fN+'.npy' #90 has 604 keys in the dictionary
 
 images=np.load(im_fN)
 aligns=np.load(align_fN)
 annots=np.load(annot_fN).item()
 
-#def create_new_outputs(output_path='../detection_outputs_new/',in_fN='../images_to_extend/image_dir_'):
-#def create_new_outputs(output_path='../detection_outputs_new/ray_output_gis/',in_fN='../images_to_extend/ray_output_gis/image_dir_'):
-#def create_new_outputs(output_path='../detection_outputs_new/ray_output_ext_2/',in_fN='../images_to_extend/ray_output_ext_2/image_dir_'):
-#def create_new_outputs(output_path='../detection_outputs_new/ray_output_new_extendby2/',in_fN='../images_to_extend/org_img_ext_2var/image_dir_'):
+def extend(pt1,pt2,change,direct):
+    if pt1[0]!=pt2[0]:
+        m = 1.0*(pt2[1]-pt1[1])/(pt2[0]-pt1[0])
+        x_ch = change * 1.0/(math.sqrt(1+m*m))
+        y_ch = change * m/(math.sqrt(1+m*m))
+    else:
+        x_ch = 0
+        y_ch = change
+    if direct == -1: #Right
+        x3 = pt2[0]+x_ch
+        y3 = pt2[1]+y_ch
+    else:
+        x3 = pt1[0]-x_ch
+        y3 = pt1[1]-y_ch
+    return [x3,y3]
 
-def create_new_outputs(output_path='../detection_outputs_new/ray_output_normal/',in_fN='../images_to_extend/ray_output_normal/image_dir_'):
+def distance(x1, x2):
+    return (math.hypot(x2[0] - x1[0], x2[1] - x1[1]))
+
+def orientation(x1, x2):
+    if float(x2[0] - x1[0]) == 0:
+        if x2[1] - x1[1] > 0:
+            return -90
+        else:
+            return 90
+    else:
+        return math.degrees(math.atan2(x2[1] - x1[1], x2[0] - x1[0]))
+
+def create_new_outputs2(output_path='../detection_outputs_new/',in_fN='../images_to_extend/image_dir_'):
     """
     THis func. calculates and saves the new detection outputs 
     """
@@ -61,59 +80,54 @@ def create_new_outputs(output_path='../detection_outputs_new/ray_output_normal/'
     for i in range(len(word_lst)):
         ori_coords = list(images[indices_lst[i]])
         saved_coords = list(ori_coords)
-        ori_coords = [ori_coords[3][0],ori_coords[3][1],ori_coords[1][0],ori_coords[1][1]]
+        #ori_coords = [ori_coords[3][0],ori_coords[3][1],ori_coords[1][0],ori_coords[1][1]]
         #Instead of directly choosing 2 and 1. Pick longer dimension
         #length_im = ori_coords[2]-ori_coords[0]
-        dim_1 = ori_coords[2]-ori_coords[0]
-        dim_2 = ori_coords[3]-ori_coords[1]
+        width = distance(ori_coords[0], ori_coords[1]) #dim_1
+        height = distance(ori_coords[0],ori_coords[3]) #dim_2
         #length_wo = len(word_lst[i])
         length_wo = word_lst[i]
-        if dim_1>dim_2:
-            length_im = dim_1
+        if width>height:
+            length_im = width
             if direct_lst[i]==1: #Increase left
-                ori_coords[0]-=int(length_im/length_wo)
-                saved_coords[0][0]-= int(length_im/length_wo)
-                saved_coords[3][0]-= int(length_im/length_wo)
+                #ori_coords[0]-=int(length_im/length_wo)
+                #saved_coords[0][0]-= int(length_im/length_wo)
+                #saved_coords[3][0]-= int(length_im/length_wo)
+                new_pt1 = extend(ori_coords[0],ori_coords[1],length_wo,1)
+                new_pt2 = extend(ori_coords[3],ori_coords[2],length_wo,1)
+                saved_coords = [new_pt1,ori_coords[1],ori_coords[2],new_pt2]
             elif direct_lst[i]==-1: #Increase to right
-                ori_coords[2]+=int(length_im/length_wo)
-                saved_coords[2][0]+= int(length_im/length_wo)
-                saved_coords[1][0]+= int(length_im/length_wo)
+                #ori_coords[2]+=int(length_im/length_wo)
+                #saved_coords[2][0]+= int(length_im/length_wo)
+                #saved_coords[1][0]+= int(length_im/length_wo)
+                new_pt1 = extend(ori_coords[0],ori_coords[1],length_wo,-1)
+                new_pt2 = extend(ori_coords[3],ori_coords[2],length_wo,-1)
+                saved_coords = [ori_coords[0],new_pt1,new_pt2,ori_coords[3]]
             elif direct_lst[i]==0: #Don't
                 ori_coords[2]*=1 #-_-
-            elif direct_lst[i]==2: #Increase left
-                ori_coords[0]-=int(length_im/length_wo)
-                saved_coords[0][0]-= 2*int(length_im/length_wo)
-                saved_coords[3][0]-= 2*int(length_im/length_wo)
-            elif direct_lst[i]==-2: #Increase to right
-                ori_coords[2]+=int(length_im/length_wo)
-                saved_coords[2][0]+= 2*int(length_im/length_wo)
-                saved_coords[1][0]+= 2*int(length_im/length_wo)
         else:
-            length_im = dim_2
+            length_im = height
             if direct_lst[i]==1: #Increase top basically
-                ori_coords[1]-=int(length_im/length_wo)
-                saved_coords[2][1]-= int(length_im/length_wo)
-                saved_coords[3][1]-= int(length_im/length_wo)
+                #ori_coords[1]-=int(length_im/length_wo)
+                #saved_coords[2][1]-= int(length_im/length_wo)
+                #saved_coords[3][1]-= int(length_im/length_wo)
+                new_pt1 = extend(ori_coords[0],ori_coords[3],length_wo,-1) #-1!
+                new_pt2 = extend(ori_coords[1],ori_coords[2],length_wo,-1)
+                saved_coords = [ori_coords[0],ori_coords[1],new_pt2,new_pt1]
             elif direct_lst[i]==-1: #Increase below basically
-                ori_coords[3]+=int(length_im/length_wo)
-                saved_coords[0][1]+= int(length_im/length_wo)
-                saved_coords[1][1]+= int(length_im/length_wo)
+                #ori_coords[3]+=int(length_im/length_wo)
+                #saved_coords[0][1]+= int(length_im/length_wo)
+                #saved_coords[1][1]+= int(length_im/length_wo)
+                new_pt1 = extend(ori_coords[0],ori_coords[3],length_wo,1) #1!
+                new_pt2 = extend(ori_coords[1],ori_coords[2],length_wo,1)
+                saved_coords = [new_pt1,new_pt2,ori_coords[2],ori_coords[3]]
             elif direct_lst[i]==0: #Don't
                 ori_coords[3]*=1 #-_-
-            elif direct_lst[i]==1: #Increase top basically
-                ori_coords[1]-=int(length_im/length_wo)
-                saved_coords[2][1]-= 2*int(length_im/length_wo)
-                saved_coords[3][1]-= 2*int(length_im/length_wo)
-            elif direct_lst[i]==-1: #Increase below basically
-                ori_coords[3]+=int(length_im/length_wo)
-                saved_coords[0][1]+= 2*int(length_im/length_wo)
-                saved_coords[1][1]+= 2*int(length_im/length_wo)
         #new_coords.append([[ori_coords[0],ori_coords[3]],[ori_coords[2],ori_coords[3]],[ori_coords[2],ori_coords[1]],[ori_coords[0],ori_coords[1]]])
         new_coords.append([[saved_coords[0][0],saved_coords[0][1]],[saved_coords[1][0],saved_coords[1][1]],[saved_coords[2][0],saved_coords[2][1]],[saved_coords[3][0],saved_coords[3][1]]])
         #new_coords.append([ori_coords[0],ori_coords[1],ori_coords[2],ori_coords[3]])
         if i%100==0:
             print(i,len(word_lst))        
-    print output_path
     np.save(output_path,new_coords)
     print("Saved at: "+output_path)
 
@@ -172,9 +186,8 @@ elif direct==-1:
     new_path=fN[:5]+'_'+str(im_id)+"_right"+'.jpg'
 test_cropping(im_path='D0090-5242001',im_id=im_id,direct=direct,word=word,new_path=new_path)
 '''
-create_new_outputs()
+create_new_outputs2()
 #test_cropping("D0006-0285025",98,1,"Paris","rm_paris_left.png","rm_paris_original.png")
-#test_cropping("D0006-0285025",93,-1,"Parp","rm_paris_left.png","rm_paris_original.png")
 #test_cropping("D0090-5242001",314,-1,"Neosho","rm_neosho_bottom.png","rm_neosho_original.png")
 #test_cropping("D0090-5242001",314,1,"Neosho","rm_neosho_top.png","rm_neosho_original.png")
 #test_cropping("D0090-5242001",314,-1,"Neosho","rm_neosho_right.png","rm_neosho_original.png",ignore_top=True)
